0433ca1502328e88ad1e5cd3474a4eaead5102f4
anuragwadhwa09 <anuragwadhwa2007@gmail.com>
2019-01-10 11:43:54 +0530

    Control Statement Program1 commited

---
+++ datatypes/program1/src/program4.java
@@ -0,0 +1,12 @@
+class program4{
+	public static void public static void main(String[] args) {
+		Long minutes = 4555222;
+		float year = minutes/525600f;
+		float months = minutes/43800f;
+		float days = minutes/1440f;
+		System.out.println("Total no. of years are:" + year);
+		System.out.println("Total no. of months are:" + months);
+		System.out.println("Total no. of days are:" + days);
+		 
+	}
+}
